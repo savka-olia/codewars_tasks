209c025d9a620a5ca2a8b2c7c20f575091ac6480
savka-olia <tokarchykolia@gmail.com>
2022-02-08 15:09:38 +0200

    updated task 7

--- kata_7.rb
+++ kata_7.rb
@@ -2,9 +2,20 @@
 # https://www.rubyguides.com/2015/08/ruby-recursion-and-memoization/
 
 def replicate(times, number)
-  return [] if times <= 0 #base step
-  res = []
-  times.times { res << number }
-  res
+  arr = []
+  if times < 1
+    arr
+  else
+    arr << number
+    arr.concat(replicate(times - 1, number))
+  end
 end
 
+# def replicate(times, number)
+#   return [] if times <= 0 #base step
+#   res = []
+#   times.times { res << number }
+#   res
+# end
+
+replicate(3, 5)
